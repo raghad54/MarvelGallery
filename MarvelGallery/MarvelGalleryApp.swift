1af53f8f5dc32af97ad42483395f3984fba5d03b
Raghad Soliman <raghad.ali.5495@gmail.com>
2024-11-13 13:32:04 +0200

    Initial Commit

---
+++ MarvelGallery/MarvelGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelGalleryApp.swift
+//  MarvelGallery
+//
+//  Created by Raghad's Mac on 13/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct MarvelGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
